d76e8e1de1a1a2968ef5cde937d018feb48e4451
Shaan Doshi <shaandoshi4@gmail.com>
2025-02-15 16:41:32 -0800

    make changes to bytes

--- upload_image_test.py
+++ upload_image_test.py
@@ -3,6 +3,8 @@ import requests
 # Read image as bytes
 with open("kids_drawing.jpg", "rb") as f:
     image_bytes = f.read()
+with open("bytes.txt", 'wb') as f2:
+    f2.write(image_bytes)
 # print(image_bytes)
 
 # Send the request
